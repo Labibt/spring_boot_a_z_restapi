3500078c30dd5f6a4209654944f542e195dfac6e
Labibt <labibt42@gmail.com>
2026-02-16 08:50:08 +0600

    validation

--- src/main/java/com/example/spring_boot_a_z_restapi/controller/StudentController.java
+++ src/main/java/com/example/spring_boot_a_z_restapi/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.example.spring_boot_a_z_restapi.controller;
 import com.example.spring_boot_a_z_restapi.dto.AddStudentRequestDto;
 import com.example.spring_boot_a_z_restapi.dto.StudentDto;
 import com.example.spring_boot_a_z_restapi.service.StudentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +27,7 @@ public class StudentController {
     }
 
     @PostMapping
-    public ResponseEntity<StudentDto> createNewStudent(@RequestBody AddStudentRequestDto addStudentRequestDto){
+    public ResponseEntity<StudentDto> createNewStudent(@RequestBody @Valid AddStudentRequestDto addStudentRequestDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(studentService.createNewStudent(addStudentRequestDto));
     }
 
@@ -37,12 +38,12 @@ public class StudentController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<StudentDto> updateStudent( @PathVariable Long id, @RequestBody AddStudentRequestDto addStudentRequestDto ){
+    public ResponseEntity<StudentDto> updateStudent( @PathVariable Long id, @RequestBody @Valid AddStudentRequestDto addStudentRequestDto ){
         return ResponseEntity.ok(studentService.updateStudent(id, addStudentRequestDto));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<StudentDto> updatePartialStudent( @PathVariable Long id, @RequestBody AddStudentRequestDto addStudentRequestDto ){
+    public ResponseEntity<StudentDto> updatePartialStudent( @PathVariable Long id, @RequestBody @Valid AddStudentRequestDto addStudentRequestDto ){
         return ResponseEntity.ok(studentService.updatePartialStudent(id, addStudentRequestDto));
     }
 
--- src/main/java/com/example/spring_boot_a_z_restapi/dto/AddStudentRequestDto.java
+++ src/main/java/com/example/spring_boot_a_z_restapi/dto/AddStudentRequestDto.java
@@ -1,10 +1,18 @@
 package com.example.spring_boot_a_z_restapi.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 public class AddStudentRequestDto {
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 30, message = "Name must be between 3 and 30 characters")
     private String name;
+
+    @Email
+    @NotBlank(message = "Email is required")
     private String email;
 }
